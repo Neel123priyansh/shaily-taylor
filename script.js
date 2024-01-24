43c43c62ae406fdbd4817d66c21bb638537f4cb9
Neel123priyansh <84672813+Neel123priyansh@users.noreply.github.com>
2024-01-24 21:42:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const h1 = document.querySelector('h1');
-const h2 = document.querySelector('h2');
-
-h1.addEventListener('mouseover', () => {
-    h2.classList.toggle('change');
-});
-h1.addEventListener('mouseout', () => {
-    h2.classList.toggle('change');
+$(document).ready(function(){
+  $('.title').click(function(){
+    $('.container').addClass('open');
+  });
+  
+  
+  $('.close').click(function(){
+    $('.container').removeClass('open');
+  });
 });
